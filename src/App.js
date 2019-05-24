6d352dd51b16a6012cdf5d0f53b092c4f0bdd6cc
AmbarishPK <41122980+Ambarishpk@users.noreply.github.com>
2019-05-24 22:56:09 +0530

    expenses backend component added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Expenses } from './components'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Expenses />
+    </div>
+  );
+}
+
+export default App;
